dcd3494ed48a434fefaf048a1dd213d0ce8ad3e5
Azshken <59659341+Azshken@users.noreply.github.com>
2023-11-03 06:07:21 +0100

    Styling update Main.js
    - text readability (custom tailwind text outline)
    - meme image size change from width (80%) to max-height (100vh)

--- memeGenerator/components/Main.js
+++ memeGenerator/components/Main.js
@@ -61,11 +61,11 @@ export default function Main() {
         </button>
       </div>
       <div class="flex flex-wrap justify-center">
-        <img src={meme.randomImage} class="w-4/5 " />
-        <h2 class="self-top absolute text-4xl text-white mt-2 font-extrabold uppercase tracking-wide shadow-inner w-4/5 text-center font-poppins ">
+        <img src={meme.randomImage} class="max-h-screen" />
+        <h2 class="self-top absolute text-4xl text-white mt-2 font-extrabold uppercase tracking-wide w-4/5 text-center font-poppins drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           {meme.topText}
         </h2>
-        <h2 class="self-end absolute text-4xl text-white mb-3 font-bold uppercase tracking-wide shadow-inner w-4/5 text-center font-poppins">
+        <h2 class="self-end absolute text-4xl text-white mb-3 font-bold uppercase tracking-wide w-4/5 text-center font-poppins drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,0.8)]">
           {meme.bottomText}
         </h2>
       </div>
